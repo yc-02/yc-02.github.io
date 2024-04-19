0fe943f6a89563e8a3bf77d0de5ac9654bc42cee
yc-02 <147283486+yc-02@users.noreply.github.com>
2024-04-19 12:24:25 -0400

    add

--- src/app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function contactPage() {
-  return (
-    <div>
-        <p>If you have any questions or feedback about Roamer, feel free to reach out to us at yingcui.roamer@gmail.com</p>
-    </div>
-  )
-}
