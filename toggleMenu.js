4439d9d621cad8306fd25ee2cb8794142ad3e1a8
yc-02 <147283486+yc-02@users.noreply.github.com>
2024-04-23 14:20:53 -0400

    updated css

--- toggleMenu.js
+++ toggleMenu.js
@@ -8,3 +8,22 @@ function closeMenu(){
   document.getElementById("openButton").style.display ='block'
   document.body.style.overflow ="scroll";
 }
+
+function toggleProjects() {
+  const projectsDiv = document.getElementById('projects');
+  if (projectsDiv.style.display === 'block') {
+      projectsDiv.style.display = 'none';
+  } else {
+      projectsDiv.style.display = 'block';
+  }
+}
+
+function toggleApps(){
+  const appsDiv = document.getElementById('apps');
+  if (appsDiv.style.display === 'block') {
+      appsDiv.style.display = 'none';
+  } else {
+      appsDiv.style.display = 'block';
+  }
+
+}
