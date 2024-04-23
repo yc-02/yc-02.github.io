98140f3859f48b0f460a5f2af8a42e1aa14c834d
yc-02 <147283486+yc-02@users.noreply.github.com>
2024-04-23 11:49:05 -0400

    updated sm screen  navbar

--- toggleMenu.js
+++ toggleMenu.js
@@ -1,6 +1,10 @@
 function openMenu() {
     document.getElementById("contents").style.display ='block'
+    document.getElementById("openButton").style.display ='none'
+    document.body.style.overflow = "hidden";
   }
 function closeMenu(){
   document.getElementById("contents").style.display ='none'
+  document.getElementById("openButton").style.display ='block'
+  document.body.style.overflow ="scroll";
 }
