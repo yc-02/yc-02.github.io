fc2af09ded293dc65d51e3fd447ec3628b39c077
yc-02 <147283486+yc-02@users.noreply.github.com>
2024-04-22 21:03:33 -0400

    updated css

---
+++ toggleMenu.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    document.getElementById("contents").style.display ='block'
+    document.getElementById('openButton').style.display='none'
+  }
+function closeMenu(){
+  document.getElementById("contents").style.display ='none'
+  document.getElementById("openButton").style.display ='block'
+}
