f4fa292106f48970d3244632bdfb27e4c8eacb42
yc-02 <147283486+yc-02@users.noreply.github.com>
2024-04-23 09:06:16 -0400

    updated navbar css

--- toggleMenu.js
+++ toggleMenu.js
@@ -1,8 +1,6 @@
 function openMenu() {
     document.getElementById("contents").style.display ='block'
-    document.getElementById('openButton').style.display='none'
   }
 function closeMenu(){
   document.getElementById("contents").style.display ='none'
-  document.getElementById("openButton").style.display ='block'
 }
