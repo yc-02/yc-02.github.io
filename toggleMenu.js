3f718924eef7bb640f2cc0771187ca7e6d8f8088
yc-02 <147283486+yc-02@users.noreply.github.com>
2024-04-23 15:02:31 -0400

    updated css

--- toggleMenu.js
+++ toggleMenu.js
@@ -1,9 +1,9 @@
-function openMenu() {
+function openNav() {
     document.getElementById("contents").style.display ='block'
     document.getElementById("openButton").style.display ='none'
     document.body.style.overflow = "hidden";
   }
-function closeMenu(){
+function closeNav(){
   document.getElementById("contents").style.display ='none'
   document.getElementById("openButton").style.display ='block'
   document.body.style.overflow ="scroll";
