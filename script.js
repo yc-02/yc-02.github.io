fbd293f520fddaf7d99b82454820deadde31e01f
yc-02 <147283486+yc-02@users.noreply.github.com>
2025-05-21 13:28:33 -0700

    updated layout

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const deskNav =
       <a href="/" class="home-link current-link">Home</a>
       <a href="/portfolio" class="portfolio-link">Portfolio</a>
       <a href="/about" class="about-link">About</a>
-      <a href="/contact" class="contact-link">Contact</a>
   </div>
 </div>
 `
@@ -18,10 +17,10 @@ const footer =
 <div class="footer-container">
   <div class="footer-left">
     <i class="fa-regular fa-copyright" color="inherit"></i>
-    <p>2024 Ying Cui</p>
+    <p>2025 Ying Cui</p>
   </div>
   <div class="footer-right">
-    <p class="web-title">Contact</p>
+    <p class="footer-title">Contact</p>
     <div class="footer-contact">
       <a href="https://linkedin.com/in/yingcui02" class="footer-email" target="_blank"><i class="fa-brands fa-linkedin"></i><p>LinkedIn</p></a>
       <a class="footer-email" href="mailto:yingcui02@gmail.com"><i class="fa-solid fa-envelope"></i><p>yingcui02@gmail.com</p></a>
@@ -48,23 +47,19 @@ const phoneNav =
     </div>
     <div style="display: flex; flex-direction: column; gap: 10px; padding: 0px 20px;">
       <a href="/" class="navIcon">
-          <i class="fa-solid fa-house" style="color:inherit"></i>          
+          <i class="fa-solid fa-house" style="color:inherit"></i>
           <p>Home</p>
       </a>
       <a href="/portfolio" class="navIcon">
         <i class="fa-solid fa-folder-open" style="color:inherit"></i>
-        <p>Portfolio</p>            
+        <p>Portfolio</p>
       </a>
       <a href="/about" class="navIcon">
-        <i class="fa-solid fa-address-card" style="color:inherit"></i>          
+        <i class="fa-solid fa-address-card" style="color:inherit"></i>
         <p>About</p>
       </a>
-      <a href="/contact" class="navIcon">
-        <i class="fa-solid fa-envelope" style="color:inherit"></i>        
-        <p>Contact</p>
-      </a>
   </div>
-  </div> 
+  </div>
 </div>
 </div>
 `
@@ -81,7 +76,7 @@ Promise.all([promise1, promise2, promise3]).then(() => {
 
 
 
-// small navbar open and close  
+// small navbar open and close
 function openNav() {
   document.getElementById("contents").style.display = 'block'
   document.getElementById("openButton").style.display = 'none'
@@ -122,15 +117,10 @@ const currentUrl = window.location.href
 const homeLink = document.querySelector('.home-link')
 const portfolioLink = document.querySelector('.portfolio-link')
 const aboutLink = document.querySelector('.about-link')
-const contactLink = document.querySelector('.contact-link')
+
 
 if (portfolioLink) {
-  if (currentUrl.endsWith('/portfolio/') ||
-    currentUrl.endsWith('/travel-blog/') ||
-    currentUrl.endsWith('/coffee-mobile/') ||
-    currentUrl.endsWith('/footwear/') ||
-    currentUrl.endsWith('/restaurant/') ||
-    currentUrl.endsWith('/recipes-blog/')) {
+  if (currentUrl.endsWith('/portfolio/')) {
     portfolioLink.classList.add('current-link')
     homeLink.classList.remove('current-link')
   } else {
@@ -146,12 +136,5 @@ if (aboutLink) {
     aboutLink.classList.remove('current-link')
   }
 }
-if (contactLink) {
-  if (currentUrl.endsWith('/contact/')) {
-    contactLink.classList.add('current-link')
-    homeLink.classList.remove('current-link')
-  } else {
-    contactLink.classList.remove('current-link')
-  }
-}
+
 
